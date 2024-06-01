ceec9dc22559b7f7ed2133a02264076a9ee66ec2
Miguel Negron <manegron@redborder.com>
2024-06-01 21:10:03 +0100

    recipes

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -1,11 +1,8 @@
-#
-# Cookbook Name:: mongodb
+# Cookbook:: mongodb
 # Recipe:: default
-#
-#
-# AFFERO GENERAL PUBLIC LICENSE V3
-#
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
 
-mongodb_config "config" do
+mongodb_config 'config' do
   action :add
 end
