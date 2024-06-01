8bf1810d58c530ecfa1df7d5e93288f5c7d97ac0
Miguel Negron <manegron@redborder.com>
2024-06-01 21:09:59 +0100

    lint providers

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -1,35 +1,32 @@
-# Cookbook Name:: mongodb
-#
+# Cookbook:: mongodb
 # Provider:: config
-#
 
 action :add do
   begin
-
     # install package
-    dnf_package "mongodb-org" do
+    dnf_package 'mongodb-org' do
       action :install
       flush_cache [ :before ]
     end
 
-    service "mongod" do
-      service_name "mongod"
+    service 'mongod' do
+      service_name 'mongod'
       ignore_failure true
-      supports :status => true, :restart => true, :enable => true
+      supports status: true, restart: true, enable: true
       action [:start, :enable]
     end
 
-    template "/etc/mongod.conf" do
-      source "mongod.conf.erb"
-      owner "root"
-      owner "root"
-      mode 0644
+    template '/etc/mongod.conf' do
+      source 'mongod.conf.erb'
+      owner 'root'
+      owner 'root'
+      mode '0644'
       retries 2
-      cookbook "mongodb"
-      notifies :restart, "service[mongod]", :delayed
+      cookbook 'mongodb'
+      notifies :restart, 'service[mongod]', :delayed
     end
 
-      Chef::Log.info("mongodb has been configured correctly.")
+    Chef::Log.info('mongodb has been configured correctly.')
   rescue => e
     Chef::Log.error(e.message)
   end
@@ -37,33 +34,31 @@ end
 
 action :remove do
   begin
-
-    service "mongod" do
+    service 'mongod' do
       ignore_failure true
-      supports :status => true, :enable => true
+      supports status: true, enable: true
       action [:stop, :disable]
     end
 
     # uninstall package
-    dnf_package "mongodb-org" do
-     action :remove
+    dnf_package 'mongodb-org' do
+      action :remove
     end
-    #
-    Chef::Log.info("mongodb has been deleted correctly.")
+
+    Chef::Log.info('mongodb has been deleted correctly.')
   rescue => e
     Chef::Log.error(e.message)
   end
 end
 
-
-action :register do #Usually used to register in consul
+action :register do
   begin
-    if !node["mongodb"]["registered"]
+    unless node['mongodb']['registered']
       query = {}
-      query["ID"] = "mongodb-#{node["hostname"]}"
-      query["Name"] = "mongodb"
-      query["Address"] = "#{node["ipaddress"]}"
-      query["Port"] = 27017
+      query['ID'] = "mongodb-#{node['hostname']}"
+      query['Name'] = 'mongodb'
+      query['Address'] = "#{node['ipaddress']}"
+      query['Port'] = 27017
       json_query = Chef::JSONCompat.to_json(query)
 
       execute 'Register service in consul' do
@@ -71,25 +66,25 @@ action :register do #Usually used to register in consul
         action :nothing
       end.run_action(:run)
 
-      node.normal["mongodb"]["registered"] = true
+      node.normal['mongodb']['registered'] = true
     end
-    Chef::Log.info("mongodb service has been registered in consul")
+    Chef::Log.info('mongodb service has been registered in consul')
   rescue => e
     Chef::Log.error(e.message)
   end
 end
 
-action :deregister do #Usually used to deregister from consul
+action :deregister do
   begin
-    if node["mongodb"]["registered"]
+    if node['mongodb']['registered']
       execute 'Deregister service in consul' do
-        command "curl -X PUT http://localhost:8500/v1/agent/service/deregister/mongodb-#{node["hostname"]} &>/dev/null"
+        command "curl -X PUT http://localhost:8500/v1/agent/service/deregister/mongodb-#{node['hostname']} &>/dev/null"
         action :nothing
       end.run_action(:run)
 
-      node.normal["mongodb"]["registered"] = false
+      node.normal['mongodb']['registered'] = false
     end
-    Chef::Log.info("mongodb service has been deregistered from consul")
+    Chef::Log.info('mongodb service has been deregistered from consul')
   rescue => e
     Chef::Log.error(e.message)
   end
